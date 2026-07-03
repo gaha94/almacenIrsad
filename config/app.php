d365fcd291c0cfd9b4634d4822361323171e1c30
gustavo <gustavo_n_c@hotmail.es>
2026-07-03 11:15:49 -0500

    Guardar gitignore y remover archivos sensibles del rastreo

--- config/app.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$host = $_SERVER['HTTP_HOST'] ?? '';
-
-$isLocal = str_contains($host, 'localhost') || str_contains($host, '127.0.0.1');
-
-define('BASE_URL', $isLocal ? '/almacen-ubicaciones' : '');
